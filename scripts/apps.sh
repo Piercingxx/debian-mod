c8e080871c08b5f3248bc129cff69d7e313682cb
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2025-09-22 20:15:23 -0400

    Update apps.sh

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -31,7 +31,7 @@ flatpak update
     flatpak install flathub net.waterfox.waterfox -y
     flatpak install flathub md.obsidian.Obsidian -y
     flatpak install flathub org.libreoffice.LibreOffice -y
-    flatpak install https://flathub.org/beta-repo/appstream/org.gimp.GIMP.flatpakref -y
+    flatpak install flathub org.gimp.GIMP -y
     flatpak install flathub org.darktable.Darktable -y
     flatpak install flathub org.gnome.SimpleScan -y
     flatpak install flathub org.blender.Blender -y
@@ -40,7 +40,6 @@ flatpak update
     flatpak install flathub io.missioncenter.MissionCenter -y
     flatpak install flathub org.kde.kdenlive -y
     flatpak install flathub io.github.shiftey.Desktop -y
-    flatpak install flathub com.discordapp.Discord -y
     flatpak install --noninteractive flathub io.github.realmazharhussain.GdmSettings -y
     flatpak install flathub com.flashforge.FlashPrint -y
     flatpak install flathub com.synology.synology-note-station -y
@@ -78,14 +77,15 @@ flatpak update
     rm synology-drive-client-15724.x86_64.deb
     sudo apt --fix-broken install -y
 
-# Steam
-    wget "https://steamcdn-a.akamaihd.net/client/installer/steam.deb"
-    wait
-    sudo dpkg -i steam.deb
-    wait
-    rm steam.deb
-    # i386 is needed for steam to launch
-    sudo dpkg --add-architecture i386
+# Steam & Discord
+    # wget "https://steamcdn-a.akamaihd.net/client/installer/steam.deb"
+    # wait
+    # sudo dpkg -i steam.deb
+    # wait
+    # rm steam.deb
+    # # i386 is needed for steam to launch
+    # sudo dpkg --add-architecture i386
+    # flatpak install flathub com.discordapp.Discord -y
 
 # Ulauncher
     gpg --keyserver keyserver.ubuntu.com --recv 0xfaf1020699503176
